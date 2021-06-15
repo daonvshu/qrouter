6bb62e77f9e7d65d859504888f2e5e788e861e93
hydrone <1286772338@qq.com>
2021-06-15 18:51:38 +0800

    add push/pop operation

--- src/qrouter.cpp
+++ src/qrouter.cpp
@@ -2,6 +2,80 @@
 
 QRouter QRouter::router;
 
-QRouter::QRouter() {
-    
+QRouter::QRouter()
+    : m_curContextId(0)
+{}
+
+QRouter& QRouter::install(QStackedWidget* stackContainer, int contextId) {
+    RouterContainerItem item;
+    item.container = stackContainer;
+    router.containers.insert(contextId, item);
+}
+
+QRouter& QRouter::of(int contextId) {
+    router.m_curContextId = contextId;
+}
+
+void QRouter::push(const QByteArray& pageClassName, const QVariant& data) {
+    Q_ASSERT_X(containers.contains(m_curContextId), "qrouter push", "cannot find context id!");
+
+    auto& item = containers[m_curContextId];
+    auto widget = reflectByName(pageClassName, item.container, data);
+    item.stack.append(widget);
+
+    item.container->addWidget(widget);
+    item.container->setCurrentWidget(widget);
+}
+
+void QRouter::pushReplace(const QByteArray& pageClassName, const QVariant& data) {
+    Q_ASSERT_X(containers.contains(m_curContextId), "qrouter push and replace", "cannot find context id!");
+
+    auto& item = containers[m_curContextId];
+    if (!item.stack.isEmpty()) {
+        auto widget = item.stack.takeLast();
+        item.container->removeWidget(widget);
+        widget->deleteLater();
+    }
+
+    push(pageClassName, data);
+}
+
+void QRouter::pop(const QVariant& data) {
+    Q_ASSERT_X(containers.contains(m_curContextId), "qrouter pop", "cannot find context id!");
+
+    auto& item = containers[m_curContextId];
+    if (!item.stack.isEmpty()) {
+        auto widget = item.stack.takeLast();
+        item.container->removeWidget(widget);
+        widget->deleteLater();
+
+        item.container->setCurrentWidget(item.stack.last());
+        item.stack.last()->onNavigateResult(data);
+    }
+}
+
+void QRouter::popUntil(const QByteArray& untilName) {
+    Q_ASSERT_X(containers.contains(m_curContextId), "qrouter pop until", "cannot find context id!");
+
+    auto& item = containers[m_curContextId];
+    while (!item.stack.isEmpty() && item.stack.last()->metaObject()->className() != untilName) {
+        auto widget = item.stack.takeLast();
+        item.container->removeWidget(widget);
+        widget->deleteLater();
+    }
+
+    if (!item.stack.isEmpty()) {
+        item.container->setCurrentWidget(item.stack.last());
+    }
+}
+
+AbstractRouterWidget* QRouter::reflectByName(const QByteArray& className, QWidget* parent, const QVariant& data) {
+    int type = QMetaType::type(className + '*');
+    auto metaObj = QMetaType::metaObjectForType(type);
+
+    auto obj = metaObj->newInstance(Q_ARG(QVariant, data), Q_ARG(QWidget*, parent));
+    AbstractRouterWidget* widget = dynamic_cast<AbstractRouterWidget*>(obj);
+    Q_ASSERT_X(widget != nullptr, "qrouter reflect page class", "cannot reflect by name!");
+
+    return widget;
 }
--- src/qrouter.h
+++ src/qrouter.h
@@ -1,20 +1,39 @@
 ﻿#pragma once
 
 #include <qobject.h>
+#include <qstackedwidget.h>
+
+#include "abstractrouterwidget.h"
 
 class QRouter {
 public:
-    static void push();
-    static void pushReplace();
-    static void popAndPush();
-    static void pushAndRemove();
 
-    static void pop();
-    static void popUntil();
+    static QRouter& install(QStackedWidget* stackContainer, int contextId = 0);
+
+    static QRouter& of(int contextId = 0);
+
+    void push(const QByteArray& pageClassName, const QVariant& data);
+    void pushReplace(const QByteArray& pageClassName, const QVariant& data);
+    void popAndPush();
+    void pushAndRemove();
+
+    void pop(const QVariant& data);
+    void popUntil(const QByteArray& untilName);
 
 private:
     QRouter();
 
 private:
     static QRouter router;
+
+    struct RouterContainerItem {
+        QStackedWidget* container;
+        QList<AbstractRouterWidget*> stack;
+    };
+
+    QHash<int, RouterContainerItem> containers;
+    int m_curContextId;
+
+private:
+    AbstractRouterWidget* reflectByName(const QByteArray& className, QWidget* parent, const QVariant& data);
 };
